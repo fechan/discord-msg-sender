039d0b51e0d4436fe41bc7b15f8bff5bc285a8c4
fechan <fred@fredchan.org>
2025-01-11 22:59:39 -0800

    add support for command line arguments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
@@ -19,18 +20,19 @@ func main() {
 	}
 
 	// Parse environment variables from command line and falling back to .env
-	botToken := os.Getenv("BOT_TOKEN")
-	channelId := os.Getenv("CHANNEL_ID")
-	message := os.Getenv("MESSAGE")
+	botTokenPtr := flag.String("bot_token", os.Getenv("BOT_TOKEN"), "ID of the channel to send to")
+	channelIdPtr := flag.String("channel_id", os.Getenv("CHANNEL_ID"), "ID of the channel to send to")
+	messagePtr := flag.String("message", os.Getenv("MESSAGE"), "The message to send")
+	flag.Parse()
 
 	// Log in as a bot
-	discord, err := discordgo.New("Bot " + botToken)
+	discord, err := discordgo.New("Bot " + *botTokenPtr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Send the message
-	_, err = discord.ChannelMessageSend(channelId, message)
+	_, err = discord.ChannelMessageSend(*channelIdPtr, *messagePtr)
 	if err != nil {
 		log.Fatal(err)
 	}
